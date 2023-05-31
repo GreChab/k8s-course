726c38084f6dbea3c2018d76e201dbddedfbe677
Grzegorz Git <grzegorz.chabiera@gmail.com>
2023-05-31 20:17:27 +0200

    Add sql scripts to init databases (create tables) to config maps.

--- user-service/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS public.t_user;
-
-CREATE TABLE t_user
-(
-    id smallserial NOT NULL,
-    username character varying(255),
-    amount character varying(255),
-    CONSTRAINT t_user_pkey PRIMARY KEY (id)
-)
