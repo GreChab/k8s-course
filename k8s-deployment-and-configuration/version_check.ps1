6f1c8d8c1bf29b23c405636ed607f0c7aef2705c
Grzegorz Git <grzegorz.chabiera@gmail.com>
2023-06-02 23:48:00 +0200

    Add Rolling-update deployment strategy

---
+++ k8s-deployment-and-configuration/version_check.ps1
@@ -0,0 +1,15 @@
+$postAppVersionUrl = "http://localhost:30001/actuator/info"
+
+while ($true) {
+    $response = Invoke-WebRequest -Uri $postAppVersionUrl
+    $responseVersion = ($response | ConvertFrom-Json).application.version
+
+    if ($responseVesion -like '1.0.8') {
+        Write-Host "POST APP Version: " $responseVersion -ForegroundColor Yellow
+    }
+    else {
+        Write-Host "POST APP Version: 1.0.4" -ForegroundColor Green
+    }
+
+    Start-Sleep -Milliseconds 500
+}
--- post-service/src/main/resources/data.sql
+++ post-service/src/main/resources/data.sql
@@ -1,5 +1,5 @@
-INSERT INTO t_post (author_id, text, posted_at) values ('1', 'Hi, this is my first post', CURRENT_DATE);
-INSERT INTO t_post (author_id, text, posted_at) values ('2', 'Hi, this is my first post', CURRENT_DATE);
-INSERT INTO t_post (author_id, text, posted_at) values ('3', 'Hi, this is my first post', CURRENT_DATE);
-INSERT INTO t_post (author_id, text, posted_at) values ('4', 'Hi, this is my first post', CURRENT_DATE);
+INSERT INTO t_post (author_id, topic, text, posted_at) values ('1', 'topic 1', 'Hi, this is my first post', CURRENT_DATE);
+INSERT INTO t_post (author_id, topic, text, posted_at) values ('2', 'topic 2', 'Hi, this is my first post', CURRENT_DATE);
+INSERT INTO t_post (author_id, topic, text, posted_at) values ('3', 'topic 3', 'Hi, this is my first post', CURRENT_DATE);
+INSERT INTO t_post (author_id, topic, text, posted_at) values ('4', 'topic 4', 'Hi, this is my first post', CURRENT_DATE);
 
